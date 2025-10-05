6c225d3bbb8431d0cbd599c5f0b15fa5038b5a68
peteculmerUoL <peteculmer@gmail.com>
2025-10-05 22:13:27 +0100

    Add ESM build
    Added ESM output to build

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -3,12 +3,34 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
+const base = {
   mode: "production",
   entry: {
     sankeyPlus: "./src/sankeyPlus.js",
   },
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({ extractComments: false })],
+  },
+  performance: {
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000,
+  },
+};
 
+// UMD build (keeps your existing behavior: window.SankeyPlus.SankeyChart)
+const umd = {
+  ...base,
   output: {
     path: path.join(__dirname, "dist"),
     publicPath: "/",
@@ -18,30 +40,24 @@ module.exports = {
       type: "umd",
     },
   },
-  target: "web",
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-      }),
-    ],
-  },
-  performance: {
-    maxEntrypointSize: 1024000,
-    maxAssetSize: 1024000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-
   plugins: [new CleanWebpackPlugin()],
 };
+
+// ESM build (real ES module bundle with named exports)
+const esm = {
+  ...base,
+  experiments: {
+    outputModule: true, // enable ESM output
+  },
+  output: {
+    path: path.join(__dirname, "distesm"),
+    publicPath: "/",
+    filename: "[name].esm.js",
+    library: {
+      type: "module", // emit ES module; export { SankeyChart } is preserved
+    },
+  },
+  // no CleanWebpackPlugin here to avoid double-clean during a multi-config build
+};
+
+module.exports = [umd, esm];	
